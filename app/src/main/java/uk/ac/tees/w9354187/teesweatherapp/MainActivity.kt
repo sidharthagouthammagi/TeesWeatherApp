c49f27e3c9c1907ce61cd20ca93aeb9ba7826b2a
Siddartha <w9354187@live.tees.ac.uk>
2023-12-12 23:16:13 +0000

    changes

--- app/src/main/java/uk/ac/tees/w9354187/teesweatherapp/MainActivity.kt
+++ app/src/main/java/uk/ac/tees/w9354187/teesweatherapp/MainActivity.kt
@@ -3,44 +3,19 @@ package uk.ac.tees.w9354187.teesweatherapp
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.material3.MaterialTheme
-import androidx.compose.material3.Surface
-import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
-import androidx.compose.ui.Modifier
-import androidx.compose.ui.tooling.preview.Preview
-import uk.ac.tees.w9354187.teesweatherapp.ui.theme.TeesWeatherAppTheme
+
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
-            TeesWeatherAppTheme {
-                // A surface container using the 'background' color from the theme
-                Surface(
-                    modifier = Modifier.fillMaxSize(),
-                    color = MaterialTheme.colorScheme.background
-                ) {
-                    Greeting("Android")
-                }
-            }
+            TeesWeatherApp()
         }
     }
 }
 
 @Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
-}
+fun TeesWeatherApp() {
 
-@Preview(showBackground = true)
-@Composable
-fun GreetingPreview() {
-    TeesWeatherAppTheme {
-        Greeting("Android")
-    }
 }
---
+++ app/src/main/java/uk/ac/tees/w9354187/teesweatherapp/navigation/WeatherScreens.kt
@@ -0,0 +1,10 @@
+package uk.ac.tees.w9354187.teesweatherapp.navigation
+
+enum class WeatherScreens {
+    SplashScreen,
+    LoginScreen,
+    RegisterScreen,
+    HomeScreen,
+    AddRoomScreen,
+    ChatRoomScreen
+}
